f8f48a6124b721681393f6ccca282185c295e3ca
Carrington <daniel@danielingraham.com>
2014-03-04 01:01:43 -0500

    getting started on TaskRules class

--- Rules/TaskRules.php
+++ Rules/TaskRules.php
@@ -9,8 +9,25 @@
  */
 class TaskRules extends AbstractRules
 {
+    
+    private $users;
+    
     public function __construct() {
         $this->configuration = array();
+        $this->users = new SplDoublyLinkedList();
+        $this->users->setIteratorMode(SplDoublyLinkedList::IT_MODE_FIFO);
+    }
+    
+    public function clearUsers() {
+        if ($this->users->isEmpty()) {
+            return;        
+        }
+        $this->users->setIteratorMode(SplDoublyLinkedList::IT_MODE_DELETE);
+        for($this->users->rewind();$this->users->valid();$this->users->next()) {
+            //My kingdom for the SplDoublyLinkedList::clear patch
+            //We're just tossing values overboard here. Nothing to see.
+        }
+        $this->users->setIteratorMode(SplDoublyLinkedList::IT_MODE_KEEP);
     }
     
     public function validateName($name) {
@@ -22,10 +39,16 @@ class TaskRules extends AbstractRules
     }
     
     public function validateInterval($interval) {
+        //validate that interval is <= minimum 
         $min = ($this->getConfig()['interval']['minimum'] <= $interval);
+        //validate that interval is >= maximum
         $max = ($interval <= $this->getConfig()['interval']['maximum']);
         return ($min && $max);
     }
+    
+    
+    
+    
 }
 
 ?>
