f0b3838b04f4e58694b9a10f9ec9d1b116886287
Carrington <daniel@danielingraham.com>
2014-03-09 03:20:46 -0400

    TaskRules fourth pass

--- src/Melete/Business/User.php
+++ src/Melete/Business/User.php
@@ -12,6 +12,38 @@ class User
     public function getDailyLimitOverride() {
         
     }
+   
+    public function getWeeklyLimitOverride() {
+        
+    }
+    
+    public function getMonthlyLimitOverride() {
+        
+    }
+    
+    public function getDailyLimit() {
+        
+    }
+    
+    public function getWeeklyLimit() {
+            
+    }
+    
+    public function getMonthlyLimit() {
+
+    }
+    
+    public function getSentToday() {
+        
+    }
+    
+    public function getSentThisWeek() {
+        
+    }
+    
+    public function getSentThisMonth() {
+    
+    }
 }
 
 ?>
