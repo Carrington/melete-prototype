7b0decc9db332711873fc8b17a0cc72a562e0330
Carrington <daniel@danielingraham.com>
2014-03-23 03:21:17 -0400

    Adding support for Eloquent

---
+++ src/Melete/ORM/Note.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Note
+ *
+ * @author Dan
+ */
+class Note
+{
+    //put your code here
+}
+
+?>
---
+++ src/Melete/ORM/Reminder.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Reminder
+ *
+ * @author Dan
+ */
+class Reminder
+{
+    //put your code here
+}
+
+?>
